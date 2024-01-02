3ebfbf2387b847971e22564cea4497ad5c562a15
esionascimento <esionascimento@proton.me>
2024-01-02 10:28:55 -0400

    feat: PerguntasFrequentes.tsx

---
+++ src/sections/perguntaFrequentes/PerguntasFrequentes.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Box } from "@mui/material";
+
+import { Title } from "@/components/Title/Title";
+
+export function PerguntasFrequentesSection() {
+  return (
+    <Box>
+      <Title>Perguntas Frequentes</Title>
+    </Box>
+  );
+}
