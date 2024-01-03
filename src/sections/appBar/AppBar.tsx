7bca899fef04dea615906d8f3e41aedc6a04e1cb
esionascimento <esionascimento@proton.me>
2024-01-02 22:12:34 -0400

    feat: AppBar section

---
+++ src/sections/appBar/AppBar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { AppBarHorizontal } from "@/components/Navbar/Horizontal";
+import { Box } from "@mui/material";
+
+export function AppBarSection() {
+  return (
+    <>
+      <Box>
+        <AppBarHorizontal />
+      </Box>
+    </>
+  );
+}
